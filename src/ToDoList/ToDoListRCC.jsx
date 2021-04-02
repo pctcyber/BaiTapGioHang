25099801e1e1cd8c3d6b5876c1dde728bc82236d
cong tin <pct.hcmut@gmail.com>
2021-04-02 10:15:32 +0700

    todolist rcc

--- src/ToDoList/ToDoListRCC.jsx
+++ src/ToDoList/ToDoListRCC.jsx
@@ -22,9 +22,6 @@ export default class ToDoListRCC extends Component {
         })
         promise.then((res) => {
 
-            // console.log('thanh cong');
-            // console.log(res.data)
-
             this.setState({
                 taskLisk: res.data
             })
@@ -36,10 +33,63 @@ export default class ToDoListRCC extends Component {
 
     }
 
+    // xu ly rejectTask
+    taskReject = (task) => {
+        
+        let promise = axios({
+            method:'PUT',
+            url:`http://svcy.myclass.vn/api/ToDoList/rejectTask?taskName=${task.taskName}`
+        })
+
+        promise.then((res) => {
+            alert(res.data);
+            this.getTaskList();
+        })
+
+        promise.catch((err) => {
+            alert(err.respone.data)
+        })
+
+    }
+
+    deletedTask = (task) => {
+
+        let promise = axios({
+            method: 'DELETE',
+            url: `http://svcy.myclass.vn/api/ToDoList/deleteTask?taskName=${task.taskName}`
+        })
+
+        promise.then(() => {
+            this.getTaskList()
+        })
+
+        promise.catch((err) => {
+            console.log(err)
+        })
+
+    }
+
+    taskDone = async (task) => {
+        
+        let promise = await axios({
+            method:"PUT",
+            url:`http://svcy.myclass.vn/api/ToDoList/doneTask?taskName=${task.taskName}`
+        })
+
+        this.getTaskList()
+
+    }
     renderTaskUncompleted = () => {
         return this.state.taskLisk.filter(task => !task.status).map((task, index) => {
-            return <li key={index}>
+            return <li key={index} style = {{ justifyContent:'normal',color:'green'}}>
                 {task.taskName}
+
+                <i className="fa fa-trash-alt" style={{ cursor: 'pointer',marginLeft:213,marginRight:10 }} onClick={() => {
+                    this.deletedTask(task)
+                }}></i>
+                <i className="fa fa-check" style = {{cursor:'pointer'}} onClick = {() => {
+                   this.taskDone(task)
+                }}></i>
             </li>
         })
 
@@ -48,6 +98,12 @@ export default class ToDoListRCC extends Component {
         return this.state.taskLisk.filter(task => task.status).map((task, index) => {
             return <li key={index}>
                 {task.taskName}
+                <i className="fa fa-trash-alt" style={{ cursor: 'pointer',marginLeft:213,marginRight:10 }} onClick={() => {
+                    this.deletedTask(task)
+                }}></i>
+                <i className="fa fa-undo" style = {{cursor:'pointer'}} onClick = {() => {
+                   this.taskReject(task)
+                }}></i>
             </li>
         })
     }
@@ -58,14 +114,14 @@ export default class ToDoListRCC extends Component {
     }
 
     handleChange = (event) => {
-        let{name,value} = event.target;
-        let newValue = {...this.state.value};
-        newValue = {...newValue,[name]:value}
+        let { name, value } = event.target;
+        let newValue = { ...this.state.value };
+        newValue = { ...newValue, [name]: value }
 
 
-        let newError = {...this.state.error};
+        let newError = { ...this.state.error };
         let regexString = /^[a-z A-Z]+$/;
-        if(!regexString.test(value) || value.trim() === ''){
+        if (!regexString.test(value) || value.trim() === '') {
 
             newError[name] = value + ' invalid !';
         } else {
@@ -75,21 +131,36 @@ export default class ToDoListRCC extends Component {
         this.setState({
             ...this.state,
             value: newValue,
-            error: newError            
-        },() => {
-           console.log(this.state); 
+            error: newError
         })
 
     }
-    
+
     addTask = (event) => {
         event.preventDefault();
+        if (this.state.error.taskName === '') {
+            let promise = axios({
+                method: 'POST',
+                url: 'http://svcy.myclass.vn/api/ToDoList/AddTask',
+                data: {
+                    taskName: this.state.value.taskName
+                }
+            })
+            promise.then(() => {
+                this.getTaskList()
 
-        
+            })
+            promise.catch((err) => {
+                alert(err.response.data)
+            })
+        } else {
+            alert('nhap lai cho dung')
+        }
+
     }
     render() {
         return (
-            <form onSubmit= {this.addTask} >
+            <form onSubmit={this.addTask} >
 
                 <div className="card">
                     <div className="card__header">
@@ -113,13 +184,13 @@ export default class ToDoListRCC extends Component {
                                 <p>September 9,2020</p>
                             </div>
                             <div className="card__add">
-                                <input onChange = {this.handleChange} id="newTask" name = 'taskName' 
-                                type="text" placeholder="Enter an activity..." /> 
-                                <button id="addItem"  onClick = {this.addTask}>
+                                <input onChange={this.handleChange} id="newTask" name='taskName'
+                                    type="text" placeholder="Enter an activity..." />
+                                <button id="addItem" onClick={this.addTask}>
                                     <i className="fa fa-plus" />
                                 </button>
                             </div>
-                                <p style = {{color:'red'}}>{this.state.error.taskName}</p>
+                            <p style={{ color: 'red' }}>{this.state.error.taskName}</p>
                             <div className="card__todo">
                                 <p style={{ color: 'green' }}>Uncompleted tasks</p>
 
